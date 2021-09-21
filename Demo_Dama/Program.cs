54f91cce55b897d5dd334bb941dfff9d96a693ab
eyp <eypsay@yahoo.com>
2021-09-21 20:56:54 +0300

    Add project files.

---
+++ Demo_Dama/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo_Dama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
